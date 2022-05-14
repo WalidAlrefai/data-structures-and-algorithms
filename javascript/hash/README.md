6de11c8defe88145c6919e59fd6fb8b07dcc44f6
WalidAlrefai <w.alrefai.3@gmail.com>
2022-05-14 12:24:42 +0300

    hashtable

---
+++ javascript/hash/README.md
@@ -0,0 +1,17 @@
+# Hashtables
+Hash tables allow the storage and retrieval of data in an average time of O(1). At its most basic level, a hash table data structure is just an array. Data is stored into this array at specific indices designated by a hash function. A hash function is a mapping between the set of input data and a set of integers.
+
+## Challenge
+Create HashTable Class in this Class create methods work with HashTable (hash , set , get , keys , contains)
+## Approach & Efficiency
+### Approach
+- reduce
+- for loop
+- while loop
+- forEach
+- if statment
+### Efficiency
+ time : O(1)
+The hash key is calculated in O(1) time complexity as always, and the required location is accessed in O(1).
+Insertion: In the best case, the key indicates a vacant location and the element is directly inserted into the hash table.
+ space : O(n) where n is the number of entries
---
+++ javascript/hash/hashTable.js
@@ -0,0 +1,73 @@
+'use strict';
+
+
+const LinkedList = require('./linkedList');
+
+
+class HashTable {
+  constructor(size){
+    this.size = size;
+    this.buckets =new Array(size);
+    this.length = 0;
+  }
+  hash(key){
+    return key.split(' ').reduce((acc, curr) => {
+      return acc + curr.charCodeAt();
+    },0)*599 % this.size;
+  }
+  set(key, val){
+    const index = this.hash(key);
+    if(!this.buckets[index]){
+      this.buckets[index] = new LinkedList();
+    }
+    let entryVal = {[key]:val};
+    this.buckets[index].append(entryVal);
+    this.length++;
+  }
+  get(key){
+    const index = this.hash(key);
+    if(!this.buckets[index]){
+      return null;
+    }
+    let current = this.buckets[index].head;
+    while(current){
+      if(current.value[key]){
+        return current.value[key];
+      }
+      current = current.next;
+    }
+    return null;
+  }
+  keys(){
+    const keys = [];
+    this.buckets.forEach(bucket => {
+      let current = bucket.head;
+      while(current){
+        for(let key in current.value){
+          keys.push(key);
+        }
+        current = current.next;
+      }
+    });
+    return keys;
+  }
+  contains(key){
+    const index = this.hash(key);
+    if(!this.buckets[index]){
+      return false;
+    }
+    let current = this.buckets[index].head;
+    while(current){
+      if(current.value[key]){
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+}
+// const hash1 = new HashTable(10);
+// hash1.set('name', 'bob');
+// hash1.set('name', 'bob');
+// console.log(hash1);
+module.exports = HashTable;
